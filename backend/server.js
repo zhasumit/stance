6088a161dd7e22b1a4dcefbd3aae2a7e1c4ac9ed
zhasumit <sumitzha2057@gmail.com>
2024-11-18 16:23:04 +0545

    auth routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse req.body
 app.use(express.urlencoded({ extended: true })); //parse form data
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
