bd9423ba6b3c3530c105be17694a4830c2d1b4c4
zhasumit <sumitzha2057@gmail.com>
2024-11-18 13:15:46 +0545

    signup functionality

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // parse req.body
+app.use(express.urlencoded({ extended: true })); //parse form data
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
